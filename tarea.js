2fd401fcbc23cf0c371e0e8ef0f63ec8bae5c282
OlgaAlarcon <olguitaalar25@gmail.com>
2023-05-22 22:04:22 -0500

    tarea-clase8

---
+++ tarea.js
@@ -0,0 +1,12 @@
+// la tarea
+
+function conteoreg(variable) {
+  for (let number = 10; number >= variable; number--) {
+    console.log(number);
+  }
+  console.log("¡DESPEGUE! 🚀");  
+}
+
+conteoreg(0);
+let cohete = "🚀 🚀 🚀";
+console.log(cohete);
